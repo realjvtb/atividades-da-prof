0a0f1288787ee8f1e868ab9a73b0fd0712399060
real_jvtb <joaotaverna26@gmail.com>
2026-05-15 15:18:28 -0300

    Add files via upload

---
+++ lista de exercicios aula 9.py
@@ -0,0 +1,18 @@
+#exerc 1
+'''import math
+
+raiz = math.sqrt(49)
+print(f"A raiz quadrada de 49 é {raiz}")'''
+
+#exerc 2
+'''def imprimir_nome(nome):
+    print(f"Olá, {nome}!")'''
+
+#exerc 3
+'''no from import voce importa o modulo todo e todas as funções do modulo,no from import
+voce importa somente a função especifica do modulo'''
+
+#exerc 4
+'''vai acontecer um conflito de nomeclatura no seu terminal,cuja ele não ira identificar o
+o modulo caso queira ser importado'''
+
